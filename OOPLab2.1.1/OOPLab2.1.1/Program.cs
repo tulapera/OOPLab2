62a5ec8920f9f9cda7f10ac835ff450a831f45ae
Віталій <154689439+tulapera@users.noreply.github.com>
2024-11-11 17:26:39 +0200

    Add files via upload

---
+++ OOPLab2.1.1/OOPLab2.1.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+namespace Lab2_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, z;
+            CultureInfo.CurrentCulture = new CultureInfo("en-US");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Введіть x: ");
+            while (!double.TryParse(Console.ReadLine(), out x))
+            {
+                Console.WriteLine("Неправильні дані, повторіть ввід для x.");
+            }
+            Console.Write("Введіть y: ");
+            while (!double.TryParse(Console.ReadLine(), out y))
+            {
+                Console.WriteLine("Неправильні дані, повторіть ввід для y.");
+            }
+            Console.Write("Введіть z: ");
+            while (!double.TryParse(Console.ReadLine(), out z))
+            {
+                Console.WriteLine("Неправильні дані, повторіть ввід для z.");
+            }
+            double result = Math.Sin(Math.Pow(y, -Math.Sqrt(Math.Abs(x)))) * (z - (x / y)) + Math.Sin(Math.Cos(z));
+            Console.WriteLine($"Результат обчислень: {result}");
+        }
+    }
+}
---
+++ OOPLab2.2.2/OOPLab2.2.2/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+
+namespace QuadraticEquationSolver
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(aTextBox.Text, out double a) && double.TryParse(bTextBox.Text, out double b) && double.TryParse(cTextBox.Text, out double c))
+            {
+                double discriminant = b * b - 4 * a * c;
+                resultTextBlock.Text = $"Дискримінант: {discriminant:F3}\n";
+
+                if (discriminant > 0)
+                {
+                    double x1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+                    double x2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+                    resultTextBlock.Text += $"Корені рівняння: x1 = {x1:F3}, x2 = {x2:F3}";
+                }
+                else if (discriminant == 0)
+                {
+                    double x = -b / (2 * a);
+                    resultTextBlock.Text += $"Єдиний корінь рівняння: x = {x:F3}";
+                }
+                else
+                {
+                    resultTextBlock.Text += "Коренів немає (дискримінант від'ємний).";
+                }
+            }
+            else
+            {
+                resultTextBlock.Text = "Помилка: неправильне значення для коефіцієнтів.";
+            }
+        }
+    }
+}
